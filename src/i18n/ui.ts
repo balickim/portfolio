3d9640e909b9f2d04fecc04cb6bf25693d6203c9
Michał Balicki <stiekerosiem@gmail.com>
2024-03-11 11:16:46 +0100

    in progress

---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+  pl: 'Polski',
+  en: 'English',
+};
+
+export const defaultLang = 'pl';
+export const showDefaultLang = false;
+
+export const ui = {
+  pl: {
+    'base.title': 'Michał Balicki | Strona domowa',
+    'base.description': 'Strona domowa Michała Balickiego',
+
+    'index.header': 'Cześć, nazywam się Michał Balicki',
+    'index.subheader': 'Jestem FullStack Web Developerem, mieszkającym obecnie w Szczecinie, Polska',
+
+    'contactCTA.title': 'Chcesz pracować razem?',
+    'contactCTA.button': 'Wyślij mi wiadomość'
+  },
+  en: {
+  },
+} as const;
